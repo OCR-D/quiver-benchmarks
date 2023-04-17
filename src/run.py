9d921edfac7f1abe4441623c4dccc5ee420287b6
Michelle Weidling <weidling@sub.uni-goettingen.de>
2023-04-17 10:45:14 +0200

    docs: run.py

--- src/run.py
+++ src/run.py
@@ -1,18 +1,36 @@
-from .constants import WORKFLOW_DIR
-from pathlib import Path
-from os import listdir
+"""
+Runs one or all given workflows in /app/workflows/ocrd_workflows
+"""
+
 import subprocess
+from os import listdir
+from pathlib import Path
+
+from .constants import WORKFLOW_DIR
+
 
 def run_workflow(workflow):
+    """Runs a given or all available workflow(s)
+
+    Args:
+        workflow (string): The name of the OCR workflow file OR 'all'
+    """
     if workflow == 'all':
         run_all_workflows()
     else:
         run_single_workflow(workflow)
 
 def run_all_workflows():
+    """Runs all OCR workflows available in workflows/ocrd_workflows
+    """
     for wf in listdir(WORKFLOW_DIR):
         pass
 
 def run_single_workflow(workflow):
+    """Runs a single OCR workflow
+
+    Args:
+        workflow (string): The name of the OCR workflow file OR 'all'
+    """
     cmd = f'bash /app/workflows/run_workflow.sh {workflow}'
     subprocess.run(cmd, shell=True, check=True)
